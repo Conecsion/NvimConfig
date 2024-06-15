d453a2f48547c0d85c5dfca85b7b10512e54bdcf
Shaodi Yang <yangshaodi21@mails.ucas.ac.cn>
2024-06-15 15:38:53 +0800

    Add autopair configs

--- lua/lazysetup.lua
+++ lua/lazysetup.lua
@@ -143,7 +143,57 @@ return {
 	{ "hrsh7th/nvim-cmp", event = "VeryLazy" },
 	{ "L3MON4D3/LuaSnip", event = "VeryLazy" },
 	{ "saadparwaiz1/cmp_luasnip", event = "VeryLazy" },
-	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+	{
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require('nvim-autopairs').setup {}
+      local npairs = require'nvim-autopairs'
+      local Rule = require'nvim-autopairs.rule'
+      local cond = require 'nvim-autopairs.conds'
+
+      local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+      npairs.add_rules {
+        -- Rule for a pair with left-side ' ' and right side ' '
+        Rule(' ', ' ')
+          -- Pair will only occur if the conditional function returns true
+          :with_pair(function(opts)
+            -- We are checking if we are inserting a space in (), [], or {}
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return vim.tbl_contains({
+              brackets[1][1] .. brackets[1][2],
+              brackets[2][1] .. brackets[2][2],
+              brackets[3][1] .. brackets[3][2]
+            }, pair)
+          end)
+          :with_move(cond.none())
+          :with_cr(cond.none())
+          -- We only want to delete the pair of spaces when the cursor is as such: ( | )
+          :with_del(function(opts)
+            local col = vim.api.nvim_win_get_cursor(0)[2]
+            local context = opts.line:sub(col - 1, col + 2)
+            return vim.tbl_contains({
+              brackets[1][1] .. '  ' .. brackets[1][2],
+              brackets[2][1] .. '  ' .. brackets[2][2],
+              brackets[3][1] .. '  ' .. brackets[3][2]
+            }, context)
+          end)
+      }
+      -- For each pair of brackets we will add another rule
+      for _, bracket in pairs(brackets) do
+        npairs.add_rules {
+          -- Each of these rules is for a pair with left-side '( ' and right-side ' )' for each bracket type
+          Rule(bracket[1] .. ' ', ' ' .. bracket[2])
+            :with_pair(cond.none())
+            :with_move(function(opts) return opts.char == bracket[2] end)
+            :with_del(cond.none())
+            :use_key(bracket[2])
+            -- Removes the trailing whitespace that can occur without this
+            :replace_map_cr(function(_) return '<C-c>2xi<CR><C-c>O' end)
+        }
+      end
+    end
+  },
 
 	{ "nvim-lua/popup.nvim" },
 
@@ -342,20 +392,20 @@ return {
 		end,
 	},
 
-	-- {
-	--   'booperlv/nvim-gomove',
-	--   event = 'VeryLazy',
-	--   config = function()
-	--     require('gomove').setup{
-	--       -- whether or not to map default key bindings, (true/false)
-	--       map_defaults = true,
-	--       -- whether or not to reindent lines moved vertically (true/false)
-	--       reindent = true,
-	--       -- whether or not to undojoin same direction moves (true/false)
-	--       undojoin = true,
-	--       -- whether to not to move past end column when moving blocks horizontally, (true/false)
-	--       move_past_end_col = false,
-	--   }
-	-- end
-	-- },
+  -- Highlight color codes like #f6c177
+  {
+    'brenoprata10/nvim-highlight-colors',
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-highlight-colors').setup({})
+    end
+  },
+
+  -- Preview LSP's goto definition and so on
+  {
+    'rmagatti/goto-preview',
+    config = function()
+      require('goto-preview').setup {}
+    end
+  }
 }
